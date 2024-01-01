41948afc0e4ef04b6f0b7b0ce5e90c2440da2534
KelvinDigit <68742854+Kelvide@users.noreply.github.com>
2024-01-01 10:15:57 -0800

    Another method to set headers and creating routes also creating html headers

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const http = require("http");
 const PORT = 3200;
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Content-Type': "application/json",
-    });
-    res.end(JSON.stringify({
-        id: 1,
-        name: "Understanding Node Server!",
-    }));
+const server = http.createServer()
+server.on('request', (req, res) => {
+    if (req.url === "/home") {
+        // res.writeHead(200, {
+        //     'Content-Type': "application/json",
+        // });
+
+        // Another method to set header
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json")
+        res.end(JSON.stringify({
+            id: 1,
+            name: "Understanding Node Server!",
+        }));
+    } else if (req.url === "/message") {
+        // Header set to Html
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html")
+        res.write('<li>Hello</li>')
+        res.write('<li>World</li>')
+        res.end();
+    } else {
+        res.statusCode = 400;
+        res.end();
+    }
 })
 
 server.listen(PORT, () => { console.log(`Listening to port ${PORT}...`) })
